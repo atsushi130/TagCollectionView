40104d0f2b9acbb29b5812b6da0c80ed92a25673
atsushi130 <atsushi_main@outlook.jp>
2018-08-26 22:01:56 +0900

    add: sample code

---
+++ TagCollectionView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TagCollectionView
+//
+//  Created by Atsushi Miyake on 2018/08/26.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var tagCollectionView: TagCollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
